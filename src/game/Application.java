6d364deb5e353411338dc2d1d114ae03fd3efea7
Sam Sweere <samsweere@gmail.com>
2019-09-16 19:00:58 +0200

    new approach with controller

--- src/game/Application.java
+++ src/game/Application.java
@@ -1,46 +1,90 @@
 package game;
 
-import java.awt.EventQueue;
+import java.awt.*;
 import java.util.concurrent.TimeUnit;
-import javax.swing.JFrame;
+import javax.swing.*;
 
-public class Application extends JFrame {
+public class Application {
     private int screenWidth = 600;
     private int screenHeight = 600;
 
+   // private menuBox onePlayerBox;
+   // private menuBox twoPlayerBox;
+
+    public int numOfPlayers = 0;
+
+
     public Application() throws InterruptedException {
+        JFrame f = new JFrame();
+        f.setLayout(new BorderLayout());
+        f.setSize(screenWidth, screenHeight);
 
-        setSize(screenWidth, screenHeight);
+        Dimension size = f.getSize();
+        int w = (int) size.getWidth();
+        int h = (int) size.getHeight();
+        f.setTitle("Adantino");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLocationRelativeTo(null);
 
-        setTitle("Adantino");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-
-        setVisible(true);
-
-        chooseGameScreen selectionScreen = new chooseGameScreen();
-        add(selectionScreen);
+        f.setVisible(true);
 
 
-        //int numOfPlayers = ;
+
+        //chooseGameScreen selectionScreen = new chooseGameScreen(w,h);
+        //f.add(selectionScreen, BorderLayout.CENTER);
+
+        //chooseGameScreen(w,h,f);
+    }
+/*
+    private void chooseGameScreen(int w, int h, JPanel f){
+        int boxWidth = 200;
+        int boxHeight = 100;
+        int boxOnePlayerX = w/2-boxWidth/2;
+        int boxOnePlayerY = h/2-boxHeight/2;
+        onePlayerBox = new menuBox(boxWidth, boxHeight, boxOnePlayerX, boxOnePlayerY, "One Player");
+        twoPlayerBox = new menuBox(boxWidth, boxHeight, boxOnePlayerX, boxOnePlayerY+2*boxHeight, "Two Players");
+        //paintComponent(g);
+        f.add(onePlayerBox);
+
+    }*/
+
+    /** Draws the triangle as this frame's painting */
+/*    public void paint(Graphics g){
+        //super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D)g;
+
+        RenderingHints rh
+                = new RenderingHints(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        rh.put(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+
+        g2d.setRenderingHints(rh);
+
+        onePlayerBox.paintBox(g2d);
+        twoPlayerBox.paintBox(g2d);
+    }
+
+    private void chooseGameScreen(){
+
     }
 
     private void twoPlayerUI() {
-        add(new Board());
+        //add(new Board());
 
 
-    }
+    }*/
 
     public static void main(String[] args) {
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            guiController ex = null;
 
-        EventQueue.invokeLater(() -> {
-            Application ex = null;
-            try {
-                ex = new Application();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            public void run() {
+                ex = new guiController();
             }
-
         });
     }
 }
---
+++ src/game/guiController.java
@@ -0,0 +1,92 @@
+package game;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.PreparedStatement;
+
+public class guiController implements ActionListener {
+    public static boolean RIGHT_TO_LEFT = false;
+
+    private static JButton onePlayerButton;
+    private static JButton twoPlayerButton;
+    private static JButton undoMoveButton;
+    private static Board board;
+
+    private AI ai;
+
+    public void addComponentsToPane(Container pane) {
+
+        if (!(pane.getLayout() instanceof BorderLayout)) {
+            pane.add(new JLabel("Container doesn't use BorderLayout!"));
+            return;
+        }
+
+        if (RIGHT_TO_LEFT) {
+            pane.setComponentOrientation(
+                    java.awt.ComponentOrientation.RIGHT_TO_LEFT);
+        }
+
+        JPanel pageStart = new JPanel();
+
+        onePlayerButton = new JButton("Player vs AI");
+        pageStart.add(onePlayerButton);
+        onePlayerButton.addActionListener(this);
+
+        twoPlayerButton = new JButton("Two Players");
+        pageStart.add(twoPlayerButton);
+        twoPlayerButton.addActionListener(this);
+
+        undoMoveButton = new JButton("Undo Move");
+        pageStart.add(undoMoveButton);
+        undoMoveButton.addActionListener(this);
+
+        pane.add(pageStart, BorderLayout.PAGE_START);
+
+        pane.add(board, BorderLayout.CENTER);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == onePlayerButton) {
+            int confirmed = JOptionPane.showConfirmDialog(null, "Are you sure you want to start a new Player vs AI game?", "New Player vs AI game", JOptionPane.YES_NO_OPTION);
+            if (confirmed == JOptionPane.YES_OPTION) {
+                System.out.println("yes option");
+            } else {
+                System.out.println("no option");
+            }
+        }
+        if(e.getSource() == twoPlayerButton) {
+            int confirmed = JOptionPane.showConfirmDialog(null, "Are you sure you want to start a new two Player game?", "New two Player game", JOptionPane.YES_NO_OPTION);
+            if (confirmed == JOptionPane.YES_OPTION) {
+                System.out.println("yes option");
+            } else {
+                System.out.println("no option");
+            }
+        }
+        if(e.getSource() == undoMoveButton){
+            System.out.println("Undo move");
+        }
+    }
+
+    public guiController(){
+        //Create and set up the window.
+        JFrame frame = new JFrame("Adantino by Sam Sweere");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create a new board
+        board = new Board();
+
+        //Create a new AI
+        ai = new AI();
+
+        //Set up the content pane.
+        addComponentsToPane(frame.getContentPane());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+}
